caaa34c0701938d0e013536efc5c51d1393c112a
SimonCreater <112795666+SimonCreater@users.noreply.github.com>
2025-04-08 19:13:17 +0900

    0.0version

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your PostgreSQL JDBC Driver? Include in your library path!");
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("PostgreSQL JDBC Driver Registered!");
+        /// if you have a error in this part, check jdbc driver(.jar file)
+
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://127.0.0.1:5432/project_restaurant", "postgres", "cse3207");
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return;
+        }
+        /// if you have a error in this part, check DB information (db_name, user name, password)
+
+        if (connection != null) {
+            System.out.println(connection);
+            System.out.println("You made it, take control your database now!");
+        } else {
+            System.out.println("Failed to make connection!");
+        }
+
+
+        /////////////////////////////////////////////////////
+        ////////// write your code on this ////////////
+        /////////////////////////////////////////////////////
+
+        connection.close();
+    }
+}
